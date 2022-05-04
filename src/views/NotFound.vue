f135dc160ea10dfc3ddd6ac0d270fac6b37b8349
horacioguz <guzmanhoracio@outlook.es>
2022-05-04 12:17:08 -0400

    final

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="text-center">
+        <img class="w-50 my-5" src="https://3kllhk1ibq34qk6sp3bhtox1-wpengine.netdna-ssl.com/wp-content/uploads/2017/12/ign-1.png" alt="">
+        <h1>La página que buscas no existe o ha ocurrido otro error.</h1>
+        <h2>Regresa a nuestra pagina principal y vuelve a intentar...</h2>
+        <router-link :to="{name: 'home'}">Volver al inicio</router-link>
+        <br>
+    </div>
+</template>
